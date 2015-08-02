34e605c0def42353987dcfb7574fb7d04dac57df
Harshay <harshay.rshah@gmail.com>
2015-08-02 17:52:39 -0500

    Add test setup and utils

---
+++ tests/utils.py
@@ -0,0 +1,25 @@
+import os
+import re
+from   copy       import deepcopy
+from   pyuiuc.url import URL
+
+url_re    = re.compile(r'{}.*'.format(URL.prefix))
+endpoints = URL.endpoints
+
+get_kv = lambda l: {k:k for k in l}
+
+def generate_valid_params():
+    for i in range(len(endpoints)):
+        yield endpoints[:i+1]
+
+def generate_invalid_params():
+    for i in xrange(4):
+        invalid = deepcopy(endpoints)
+        del invalid[i]
+        yield invalid
+
+def get_xml_body():
+    dpth = os.path.dirname(os.path.abspath(__file__))
+    pth = os.path.join(dpth, 'test.xml')
+    with open(pth, 'r') as f: content = f.read()
+    return content
