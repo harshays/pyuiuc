8c9cb187e61ee1183fffe43feab48400353187a0
Harshay <harshay.rshah@gmail.com>
2015-08-02 21:23:15 -0500

    Add magic method, attributes and find_by_attr to Tag class

--- pyuiuc/tag.py
+++ pyuiuc/tag.py
@@ -59,15 +59,20 @@ class Tag(object):
         if not self.made_request: self.make_request()
         return self._element
 
+    @property
+    def attributes(self):
+        if not self.made_request: self.make_request()
+        return self._attributes
+
     def make_request(self):
         '''
         makes request to get xml content and creates XML.element object
         '''
         self._request = requests.get(self.url.url)
         self._text    = self._request.text
-
         try:
             self._element     = xml.fromstring(self._text)
+            self._attributes  = self._element.attrib
             self.made_request = True
         except xml.ParseError:
             raise InvalidParametersError("One or more parameters are invalid")
@@ -117,7 +122,7 @@ class Tag(object):
 
     def find(self, tag_name=None, tag_type=None, tagify=True):
         '''
-        returns a list of descendants, optinally filtered by tag and tag type
+        returns a list of descendants, optionally filtered by tag and tag type
         converts 'endpoint' xml elements to Tag objects
 
         optional keyword arguments 
@@ -145,12 +150,36 @@ class Tag(object):
 
         return filtered
 
-    def __str__(self):
-        return "Tag object of URL {}".format(self.url.url)
+    # change to use from_element
+    def find_by_attributes(self, tag_name=None, tagify=True, **attributes):
+        '''
+        returns the first descendants, that matches tag_name and attributes
+        converts 'endpoint' xml elements to Tag objects
 
-    def __eq__(self, other):
-        if not isinstance(other, type(self)): return False
-        return self.url.__eq__(other.url)
+        optional keyword arguments
+            - tag_name    name of tag
+            - attributes  dict of attributes to filter by 
+            - tagify      converts to Tag objects. defaults to True
+        '''
+        all_dict = self.get_grouped_children(group_by='tag', tagify=False)
+
+        tag_filtered = all_dict.get(tag_name, []) if tag_name \
+                       else sum(all_dict.values(), [])
+
+        filtered = []
+
+        for elem in tag_filtered:
+            for attr, val in attributes.items():
+                if elem.attrib.get(attr, None) == str(val):
+                    filtered.append(elem)
+
+        if tagify: 
+            self.tagify(filtered)
+
+        if filtered:
+            return filtered[0]
+
+        return
 
     @staticmethod
     def _children(element, lst=[]):
@@ -162,3 +191,13 @@ class Tag(object):
             if list(ch):
                 Tag._children(ch, lst)
         return lst
+
+    def __str__(self):
+        return "{} object of URL {}".format(type(self).__name__, self.url.url)
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)): return False
+        return self.url.__eq__(other.url)
+
+    def __getitem__(self, key):
+        return self.attributes.__getitem__(key)
